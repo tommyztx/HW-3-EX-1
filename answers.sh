ea1864cdd75f9958cca745bfbd73f0ba4e246d2c
Tommy Zheng <tommyztx@gmail.com>
2024-09-30 13:26:19 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -2,8 +2,8 @@ SELECT Name FROM Students;
 SELECT * FROM Students WHERE Age > 30;
 SELECT Name FROM Students WHERE Gender='F' AND Age=30;
 SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
+INSERT INTO Students VALUES(7, 'Thomas Zheng', '21', 'M', 500);
+UPDATE Students SET Points = 400 WHERE Name='Basma';
 UPDATE Students SET Points = 100 WHERE Name='Alex';
 
 CREATE TABLE graduates(
