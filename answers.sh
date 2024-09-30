7716d3f1be6cf3a88fd97410daf413b4e737db44
Tommy Zheng <tommyztx@gmail.com>
2024-09-30 18:41:46 -0500

    Joins

--- answers.sh
+++ answers.sh
@@ -19,9 +19,10 @@ INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Point
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
 DELETE FROM students WHERE Name='Layal';
 
-SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
-SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
-SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
+SELECT employees.Name, companies.Name, companies.Date FROM Employees JOIN Companies ON Employees.Company = Companies.Name;
+SELECT employees.Name FROM Employees JOIN Companies ON Employees.Company =  Companies.Name WHERE Companies.Date < 2000;
+SELECT companies.Name FROM companies JOIN employees ON companies.Name = employees.Company WHERE Employees.Role = "Graphic Designer";
+
 
 select name from students where points=(SELECT MAX(points) from students);
 select AVG(points) from students;
